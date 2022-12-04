e38fe7c6bf16f4615bccb816cf6e7cf6edfd6296
Denis <ivdenvas@gmail.com>
2022-12-04 15:56:33 +0300

    ?

---
+++ Task 47/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+int m = userInput("Введите количество строк: ", "Ошибка ввода!");
+int n = userInput("Введите количество столбцов: ", "Ошибка ввода!");
+double[,] array = GetArray(m, n, -50, 50);
+
+PrintArray(array);
+
+
+static int userInput (string message, string message1)
+{
+    while(true)
+    {
+        Console.Write(message);
+        if(int.TryParse(Console.ReadLine(), out int num))
+            return num;
+        Console.WriteLine(message1);
+    }
+}
+
+double[,] GetArray(int m, int n, double minValue, double maxValue)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = Math.Round(new Random().NextDouble() * 100, 2);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
